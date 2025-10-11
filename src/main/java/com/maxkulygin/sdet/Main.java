6583e7e073a65ef689a9d307dbe033736ce9766f
Maxim Kulygin <akademuk97@gmail.com>
2025-10-11 19:29:39 +0700

    initial selenium

--- src/main/java/com/maxkulygin/sdet/Main.java
+++ src/main/java/com/maxkulygin/sdet/Main.java
@@ -1,7 +1,18 @@
 package com.maxkulygin.sdet;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("https://www.google.com");
+            String title = driver.getTitle();
+            System.out.println("Page Title is: " + title);
+            driver.quit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
