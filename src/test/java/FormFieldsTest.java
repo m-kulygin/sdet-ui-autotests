6917b5513a675da58be73a1fda73c0c263071876
Maxim Kulygin <akademuk97@gmail.com>
2025-10-14 11:32:55 +0700

    Custom testcases with autotests added

--- src/test/java/FormFieldsTest.java
+++ src/test/java/FormFieldsTest.java
@@ -1,11 +1,14 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class FormFieldsTest {
 
@@ -14,10 +17,12 @@ public class FormFieldsTest {
 
     @Before
     public void setUp() {
-//        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://practice-automation.com/form-fields/");
         formFieldsPage = new FormFieldsPage(driver);
+        this.driver = driver;
     }
 
     @Test
@@ -40,8 +45,47 @@ public class FormFieldsTest {
         alert.accept();
     }
 
+    @Test
+    public void testFormSubmissionWithRequiredOnly() {
+        formFieldsPage.fillName("Max Kulygin")
+                .clickSubmitButton();
+
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        assertEquals("Message received!", alertText);
+        alert.accept();
+    }
+
+    @Test
+    public void testFormSubmissionWithInvalidEmail() {
+        formFieldsPage.fillName("Max Kulygin")
+                .fillEmail("pochta.mailru")
+                .clickSubmitButton();
+
+        assertFalse(isAlertPresent());
+    }
+
+    @Test
+    public void testFormSubmissionWithEmptyName() {
+        formFieldsPage.fillPassword("12345678")
+                .fillEmail("qwe@email.ru")
+                .fillMessage("Nemo")
+                .clickSubmitButton();
+
+        assertFalse(isAlertPresent());
+    }
+
     @After
     public void tearDown() {
         driver.quit();
+    }
+
+    private boolean isAlertPresent() {
+        try{
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
     }
 }
